89ac0ea08107914ac625cb526d644e5d6bab5378
Sergio Dominguez <sergiodr98jc@gmail.com>
2022-06-07 17:48:29 -0500

    Add healthcheck

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const { version } = require('./package.json')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
+
+app.get('/health', (_, res) => {
+  res.send('OK')
+})
+
+app.get('/version', (_, res) => {
+  res.send(version)
+})
 
 app.use(express.static('dist'))
 
